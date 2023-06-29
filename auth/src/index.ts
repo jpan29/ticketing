4671a5dbe9d0d49cd682f5a8b9ec493bb95c401d
Jie Pan <jiepan29@gmail.com>
2023-06-30 00:30:13 +1000

    added start message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 import { app } from './app'
 
 const start = async () => {
+  console.log('Starting up..')
+
   if (!process.env.JWT_KEY) throw new Error('Jwt must be defined')
   if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined')
   try {
